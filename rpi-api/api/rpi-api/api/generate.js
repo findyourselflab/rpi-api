0fa664fc7044ebfdc60ea92680984f2b5b8a7edc
findyourselflab <76199048+findyourselflab@users.noreply.github.com>
2026-02-25 16:28:34 +0800

    Delete rpi-api/api/rpi-api/api directory

--- rpi-api/api/rpi-api/api/generate.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  const expire = Math.floor(Date.now() / 1000) + 30 * 60;
-  const link = `https://findyourselflab.github.io/cc/?expire=${expire}`;
-  res.status(200).send(link);
-}
