6d76877b96a0c67cd86616c8f65fc7e3d0677e38
findyourselflab <76199048+findyourselflab@users.noreply.github.com>
2026-02-25 16:40:47 +0800

    Add files via upload

--- rpi-api/api/generate.js
+++ rpi-api/api/generate.js
@@ -1,5 +1,5 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
   const expire = Math.floor(Date.now() / 1000) + 30 * 60;
   const link = `https://findyourselflab.github.io/cc/?expire=${expire}`;
   res.status(200).send(link);
-}
+};
