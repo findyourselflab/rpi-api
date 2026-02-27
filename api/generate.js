099ffc946647d37da54518eec647854851fbe6e8
findyourselflab <76199048+findyourselflab@users.noreply.github.com>
2026-02-27 16:34:03 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -25,8 +25,8 @@ export default function handler(req, res) {
       // 天赋自测
       link = `${baseDomain}tianfu.html?expire=${expire}`;
       break;
-    case 'beauty':
-      link = `${baseDomain}beauty.html?expire=${expire}`;
+    case 'Beauty':
+      link = `${baseDomain}Beauty.html?expire=${expire}`;
       break;
     default:
       // 默认返回 rpi
@@ -35,5 +35,6 @@ export default function handler(req, res) {
   
   res.status(200).send(link);
 }
+
 
 
